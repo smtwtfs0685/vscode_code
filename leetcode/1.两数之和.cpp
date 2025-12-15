350389e599f7e24c520d6c19664c19bdde14d7e9
smtwtfs0685 <hzj0685@outlook.com>
2025-12-15 23:06:34 +0800

    smtwtfs

--- leetcode/1.两数之和.cpp
+++ leetcode/1.两数之和.cpp
@@ -5,10 +5,21 @@
  */
 
 // @lc code=start
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        for(int i=0 ; i<nums.size()-1 ; i++){
+            for(int j=i+1 ; j<nums.size() ; j++){
+                if(nums[i] + nums[j] == target){
+                    return {i,j} ;
+                }
+            }
+        }
+        return {} ;
     }
 };
 // @lc code=end
